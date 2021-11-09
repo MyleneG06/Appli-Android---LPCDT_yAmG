99c1e803091facf0b77761fd678d03e263b68d03
Mylène GERVAISE <mylene.gervaise@gmail.com>
2021-11-09 08:25:55 +0100

    créa projet

---
+++ app/src/main/java/com/example/romanticruise/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,48 @@
+package com.example.romanticruise.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.support.v4.app.Fragment
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import com.example.romanticruise.R
+import com.example.romanticruise.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(
+                this,
+                ViewModelProvider.NewInstanceFactory()
+            ).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/romanticruise/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,47 @@
+package com.example.romanticruise.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.support.v4.app.Fragment
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import com.example.romanticruise.R
+import com.example.romanticruise.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+                NotificationsViewModel::class.java
+            )
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
